9ba7edae05ce4a3015031d96f530f4c03a8666a2
matiasdeveloper <matiasdeveloper@outlook.com>
2020-10-21 20:01:36 -0300

    Neural Network with one hidden layer
    Two folders:
    - Programming Lab (Is the course)
    - PythonVS (Is the personal working)

--- Week 3/Python VScode/NN_Test_2units.py
+++ Week 3/Python VScode/NN_Test_2units.py
@@ -6,6 +6,7 @@ import sklearn.datasets
 import sklearn.linear_model
 from Utilities.planar_utils import plot_decision_boundary, load_planar_dataset, load_extra_datasets
 from NeuralNetwork import NeuralNetwork
+from Utilities.ColorConsole import bcolors
 
 # General arquitecture for nn one hidden layer
 # Random init
@@ -15,25 +16,27 @@ from NeuralNetwork import NeuralNetwork
     # Backward propagation
     # Update parameters
 # Predict graphics
-print('Neural network with one hidden layer and -n- units')
-print('-----------------------------')
-print('-----------------------------')
-print('Developed by Matias Vallejos with the course NN deepleraning.ai')
-print('No linear problem')
-print('-----------------------------')
+print('')
+print(bcolors.BOLD + bcolors.UNDERLINE + 'Neural network with one hidden layer and -n- units' + bcolors.ENDC)
+print(bcolors.OKGREEN + '>> Developed by Matias Vallejos <<' + bcolors.ENDC)
+print('Course deeplearning.ai (Bibliography)')
 
-input("Press Enter to continue...")
+print('')
+print(bcolors.OKCYAN + '|-------------<>--------------|' + bcolors.ENDC)
+input(bcolors.WARNING +'Press enter to plot data..' + bcolors.ENDC)
+print(bcolors.OKCYAN + '|-------------<>--------------|' + bcolors.ENDC)
 
-print('-----------------------------')
-print('Plotting data...')
-print('-----------------------------')
-
+print('View data in the graph')
+print('The X and Y axis in spectral mode.')
 X, Y = load_planar_dataset()
 plt.scatter(X[0, :], X[1, :], c=Y, s=40, cmap=plt.cm.Spectral)
 plt.show()
 
-input("Press enter to get data from dataset")
-print('-----------------------------')
+print('')
+print(bcolors.OKCYAN + '|-------------<>--------------|' + bcolors.ENDC)
+input(bcolors.WARNING +'Press enter to get data..' + bcolors.ENDC)
+print(bcolors.OKCYAN + '|-------------<>--------------|' + bcolors.ENDC)
+
 # Sizes
 shape_X = X.shape
 shape_Y = Y.shape
@@ -42,32 +45,45 @@ m = X.shape[1]  # training set size
 print ('The shape of X is: ' + str(shape_X))
 print ('The shape of Y is: ' + str(shape_Y))
 print ('I have m = %d training examples!' % (m))
-print('-----------------------------')
 
-def layer_sizes(X, Y, u):
-    n_x = X.shape[0]
-    n_y = Y.shape[0]
-    n_h = u
-    return (n_x, n_y, n_h)
+print('')
+print(bcolors.OKCYAN + '|-------------<>--------------|' + bcolors.ENDC)
+input(bcolors.WARNING + 'Press enter to training model..' + bcolors.ENDC)
+print(bcolors.OKCYAN + '|-------------<>--------------|' + bcolors.ENDC)
 
-input("Press enter to training model..")
-print('-----------------------------')
-NeuralN = NeuralNetwork(layer_sizes(X, Y, 4), X, Y)
-parameters = NeuralN.train_model(10000, 1.2, True)
-print('-----------------------------')
+# Training the neural network with one hidden layer
+NeuralN = NeuralNetwork(X, Y)
+parameters = NeuralN.train_model(4, 10000, 1.12, True)
 
-print('Press enter to visualize learn parameters..')
-print('-----------------------------')
+print('')
+print(bcolors.OKCYAN + '|-------------<>--------------|' + bcolors.ENDC)
+print(bcolors.WARNING + 'Press enter to visualize learned parameters..' + bcolors.ENDC)
+print(bcolors.OKCYAN + '|-------------<>--------------|' + bcolors.ENDC)
+
+# Print the learned parameters
 print("W1 = " + str(parameters["W1"]))
 print("b1 = " + str(parameters["b1"]))
 print("W2 = " + str(parameters["W2"]))
 print("b2 = " + str(parameters["b2"]))
-print('-----------------------------')
 
-print('Press enter to plot learn parameters')
-print('-----------------------------')
+print('')
+print(bcolors.OKCYAN + '|-------------<>--------------|' + bcolors.ENDC)
+input(bcolors.WARNING + 'Press enter to visualize training accuracy' + bcolors.ENDC)
+print(bcolors.OKCYAN + '|-------------<>--------------|' + bcolors.ENDC)
 
-"""
-plot_decision_boundary(lambda x: NeuralN.predict(NeuralN.X_train.T), X, y)
+predictions = NeuralN.predict(parameters, X)
+NeuralN.accuracy(predictions)
+
+print('')
+print(bcolors.OKCYAN + '|-------------<>--------------|' + bcolors.ENDC)
+input(bcolors.WARNING + 'Press enter to plot learn parameters' + bcolors.ENDC)
+print(bcolors.OKCYAN + '|-------------<>--------------|' + bcolors.ENDC)
+
+print('View data in the graph')
+print('Visualize the parameters learned with the neural network')
+print('The neural resolved a one problem of non linear random data')
+
+# Plot the leraned parameters in a graphics
+plot_decision_boundary(lambda x: NeuralN.predict(parameters, x.T), X, Y)
 plt.title("Decision Boundary for hidden layer size " + str(4))
-plt.show() """
+plt.show()
--- Week 3/Python VScode/NeuralNetwork.py
+++ Week 3/Python VScode/NeuralNetwork.py
@@ -1,52 +1,58 @@
 import numpy as np
+from Utilities.ColorConsole import bcolors
 
 class NeuralNetwork:
-    def __init__(self, layer_sizes, X_train, y_train):
+    def __init__(self, X_train, y_train):
+        # Training examples
         self.X = X_train
         self.Y = y_train
-
-        self.layer_sizes = layer_sizes
-        self.w1, self.w2, self.b1, self.b2 = self.random_init()
-
+        # One hidden layer and one output layer
+        self.W1 = []
+        self.b1 = []
+        self.W2 = []
+        self.b2 = []
+        # Historic parameters
         self.parameters_hist = []
         self.cost_hist = []
         self.iters_hist = []
         
-    
-    def random_init(self):
-        n_x = self.layer_sizes[0] 
-        n_y = self.layer_sizes[1]
-        n_h = self.layer_sizes[2]
+    def layer_sizes(self, X, n_h, Y):
+        n_x = X.shape[0]
+        n_h = n_h
+        n_y = Y.shape[0]
 
-        print("The size of the input layer is: n_x = " + str(n_x))
-        print("The size of the hidden layer is: n_h = " + str(n_h))
-        print("The size of the output layer is: n_y = " + str(n_y))
-        print('-----------------------------')
+        print('Initialize neural network with one hidden layer with this sizes: ')
+        print(" - The size of the input layer is: n_x = " + str(n_x))
+        print(" - The size of the hidden layer is: n_h = " + str(n_h))
+        print(" - The size of the output layer is: n_y = " + str(n_y))
+        print(bcolors.OKCYAN + '|-------------<>--------------|' + bcolors.ENDC)
+        return (n_x, n_h, n_y)
 
+    def random_init(self, n_x, n_h, n_y):
         # Random initialize parameters for one hidden layer
         np.random.seed(2)  # we set up a seed so that your output matches ours although the initialization is random.
 
-        w1 = np.random.randn(n_h, n_x) * 0.01
-        b1 = np.zeros(shape=(n_h, 1))
-        w2 = np.random.randn(n_y, n_h) * 0.01
-        b2 = np.zeros(shape=(n_y, 1))
+        self.W1 = np.random.randn(n_h, n_x) *0.01
+        self.b1 = np.zeros(shape=(n_h, 1))
+        self.W2 = np.random.randn(n_y, n_h) *0.01
+        self.b2 = np.zeros(shape=(n_y, 1))
 
         print('Random initialize parameters:')
-        print("W1 = " + str(w1))
-        print("b1 = " + str(b1))
-        print("W2 = " + str(w2))
-        print("b2 = " + str(b2))
-        print('-----------------------------')
+        print("W1 = " + str(self.W1))
+        print("b1 = " + str(self.b1))
+        print("W2 = " + str(self.W2))
+        print("b2 = " + str(self.b2))
+        print(bcolors.OKCYAN + '|-------------<>--------------|' + bcolors.ENDC)
         
-        assert (w1.shape == (n_h, n_x))
-        assert (b1.shape == (n_h, 1))
-        assert (w2.shape == (n_y, n_h))
-        assert (b2.shape == (n_y, 1))
+        parameters = {"W1": self.W1,
+                    "b1": self.b1,
+                    "W2": self.W2,
+                    "b2": self.b2}
 
-        return w1, w2, b1, b2
-    
-    def sigmoid(self, x):
-        s = 1 / (1 + np.exp(-x))
+        return parameters
+        
+    def sigmoid(self, z):
+        s = 1 / (1 + np.exp(-z))
         return s
 
     def tanh(self,x):
@@ -54,15 +60,11 @@ class NeuralNetwork:
         return t
 
     def forward(self, X):
-        w1 = self.w1
-        b1 = self.b1
-        w2 = self.w2
-        b2 = self.b2
 
         # Implement forward propagation to calculate A2
-        Z1 = np.matmul(w1, X) + b1
-        A1 = np.tanh(Z1)
-        Z2 = np.matmul(w2, A1) + b2
+        Z1 = np.dot(self.W1, X) + self.b1
+        A1 = self.tanh(Z1)
+        Z2 = np.dot(self.W2, A1) + self.b2
         A2 = self.sigmoid(Z2)
 
         cache = {"Z1": Z1,
@@ -76,27 +78,25 @@ class NeuralNetwork:
         m = Y.shape[1]
         
         # Compute the cross-entropry cost
-        logprobs = (1/m)*np.sum(np.multiply(np.log(A2),Y) +np.multiply(np.log(1-A2),(1-Y)))
-        cost = - np.sum(logprobs)
+        logprobs = np.multiply(np.log(A2), Y) + np.multiply((1-Y), np.log(1-A2))
+        cost = - np.sum(logprobs) / m
+        
         # makes sure cost is the dimension we expect. 
-        # E.g., turns [[17]] into 17 
-        cost = float(np.squeeze(cost))
-        assert(isinstance(cost, float))
+        cost = float(np.squeeze(cost))     # E.g., turns [[17]] into 17 
         
         return cost
     
     def backpropagation(self, cache, X, Y):
         m = X.shape[1]
-        w1 = self.w1
-        w2 = self.w2
 
         A1 = cache['A1']
         A2 = cache['A2']
+        
         # Implement the backward propagation 
         dZ2 = A2 - Y
         dW2 = (1 / m) * np.dot(dZ2, A1.T)
         db2 = (1 / m) * np.sum(dZ2, axis=1, keepdims=True)
-        dZ1 = np.multiply(np.dot(w2.T, dZ2), 1 - np.power(A1, 2))
+        dZ1 = np.multiply(np.dot(self.W2.T, dZ2), 1 - np.power(A1, 2))
         dW1 = (1 / m) * np.dot(dZ1, X.T)
         db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
 
@@ -107,25 +107,36 @@ class NeuralNetwork:
 
         return grads
     
-    def update_parameters(self, grads, lr):
+    def update_parameters(self, grads, learning_rate):
+        
         dW1 = grads['dW1']
         db1 = grads['db1']
         dW2 = grads['dW2']
         db2 = grads['db2']
-        # Update rule
-        self.w1 = self.w1 - lr * dW1
-        self.b1 = self.b1 - lr * db1
-        self.w2 = self.w2 - lr * dW2
-        self.b2 = self.b2 - lr * db2
         
-        parameters = {"W1": self.w1,
+        # Update rule
+        self.W1 = self.W1 - (learning_rate * dW1)
+        self.b1 = self.b1 - (learning_rate * db1)
+        self.W2 = self.W2 - (learning_rate * dW2)
+        self.b2 = self.b2 - (learning_rate * db2)
+        
+        parameters = {"W1": self.W1,
                 "b1": self.b1,
-                "W2": self.w2,
+                "W2": self.W2,
                 "b2": self.b2}
 
         return parameters
 
-    def train_model(self, iters=10000, lr=1.2, print_cost=False):
+    def train_model(self, l_units,iters=10000, lr=1.2, print_cost=False):
+        np.random.seed(3)
+        sizes = self.layer_sizes(self.X, l_units, self.Y)
+        n_x = sizes[0]
+        n_y = sizes[2]
+        n_h = sizes[1]
+        
+        # Random initialize
+        parameters = self.random_init(n_x, n_h, n_y)
+
         #Loop gradient descent
         for i in range(0, iters):
             # Forward propagation
@@ -135,19 +146,23 @@ class NeuralNetwork:
             # Backward propagation
             grads = self.backpropagation(cache, self.X, self.Y)
             # Update parameters
-            self.parameters_hist = self.update_parameters(grads, lr)
+            parameters = self.update_parameters(grads, lr)
             
             # Print the cost every 1000 iterations
             if print_cost and i % 500 == 0:
-                print ("Cost after iteration %i: %f" %(i, cost))
+                print (bcolors.FAIL + "Cost after iteration %i: %f" %(i, cost) + bcolors.ENDC)
                 self.cost_hist.append(cost)
                 self.iters_hist.append(i)
+                self.parameters_hist.append(parameters)
                 
-        return self.parameters_hist
+        return parameters
 
-    def predict(self, X):
+    def predict(self, parameters, X):
         # Computes probabilities using forward propagation, and classifies to 0/1 using 0.5 as the threshold.
         A2, cache = self.forward(X)
-        predictions = A2 > 0.5
+        predictions = np.round(A2)
 
         return predictions
+    
+    def accuracy(self, predictions):
+        print ('Accuracy: %d' % float((np.dot(self.Y,predictions.T) + np.dot(1- self.Y, 1-predictions.T))/float(self.Y.size)*100) + '%')
